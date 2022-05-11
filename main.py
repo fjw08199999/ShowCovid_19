cc3143da51a7f6c9948f44d12d53e0377984e8b0
Fred.Fu <fjw08199999@gmail.com>
2022-05-11 15:06:44 +0800

    新增Main.py的Code

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import pandas as pd
+from tkinter import *
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 視窗設定
+windows = tk.Tk()
+windows.geometry("400x300")
+var = tk.StringVar()
+varSearchData = tk.StringVar()
+
+# Database
+dataArray: [str] = [
+    "桃園市",
+    "台北市",
+    "新北市",
+    "彰化縣",
+    "員林市",
+    "新竹縣",
+    "基隆市"
+]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_api(conmane):
+    # 抓取Covid19 API
+    r = requests.get(
+        "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5001&limited=" + conmane + "")
+    dataList = r.json()
+
+    for i in dataList:
+        print(i["id"], i["a01"], i["a02"], i["a03"], i["a04"], i["a05"], i["a06"], i["a07"])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# DisplayText
+buttonText: str = "搜尋"
+titleLabelText: str = "請輸入軟體名稱:"
+messageboxTitle: str = "message"
+errorMessage: str = "請輸入軟體名稱"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# HostInfo
+databasePath: str = "//10.152.110.2/pcinfo/INPCTW-PCIESSDCN.html"
+databaseIP: str = "10.152.110.2"
+
+
+# FunctionArea
+def get_input_text():
+    if inputTextField.get() == "":
+        tk.messagebox.showerror(messageboxTitle, errorMessage)
+    else:
+        varSearchData.set(inputTextField.get())
+        get_api(inputTextField.get())
+        # canvas.forget()
+
+
+# View
+titleLabelTextLabel = tk.Label(windows, text=titleLabelText)
+inputTextField = ttk.Combobox(windows, values=dataArray)
+searchButton = tk.Button(windows, text=buttonText, height=1, command=get_input_text)
+displayTextLabel = tk.Label(windows, textvariable=varSearchData)
+
+titleLabelTextLabel.place(x=10, y=15)
+inputTextField.place(x=120, y=15)
+searchButton.place(x=300, y=10)
+displayTextLabel.place(x=10, y=40)
+
+# searchData
+testText = pd.read_html(databasePath)
+
+# databasePathFor testText in varSearchData:
+
+# print(testText)
+
+# 圖片顯示隱藏
+# canvas = Canvas(windows, width=300, height=300)
+# canvas.pack()
+# img = PhotoImage(file="C:/Users/fred.fu/Desktop/aaaa.png")
+# canvas.create_image(150, 200, image=img)
+
+windows.mainloop()
