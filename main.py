20973ea5b048f9736f99b08b848e3bafb41e5171
Fred.Fu <fjw08199999@gmail.com>
2022-05-11 18:12:31 +0800

    實作處理跌代問題 把所有資料完整顯示出來

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import pandas as pd
-from tkinter import *
+# from tkinter import *
 import requests
 
 # 視窗設定
 windows = tk.Tk()
-windows.geometry("400x300")
+windows.geometry("600x300")
 var = tk.StringVar()
-varSearchData = tk.StringVar()
+varSearchData01 = tk.StringVar()
+varSearchData02 = tk.StringVar()
+varSearchData03 = tk.StringVar()
+varSearchData04 = tk.StringVar()
+varSearchData05 = tk.StringVar()
+varSearchData06 = tk.StringVar()
+varSearchData07 = tk.StringVar()
 
 # Database
 dataArray: [str] = [
@@ -23,47 +29,66 @@ dataArray: [str] = [
 ]
 
 
-def get_api(conmane):
-    # 抓取Covid19 API
+# FunctionArea
+def get_input_text():
+    if inputTextField.get() == "":
+        tk.messagebox.showerror(messageboxTitle, errorMessage)
+    else:
+        dataListaa = get_api(inputTextField.get())
+
+# TODO 實作處理跌代問題 把所有資料完整顯示出來
+# 抓取Covid19 API
+def get_api(conname):
     r = requests.get(
-        "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5001&limited=" + conmane + "")
+        "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5001&limited=" + conname + "")
     dataList = r.json()
 
     for i in dataList:
-        print(i["id"], i["a01"], i["a02"], i["a03"], i["a04"], i["a05"], i["a06"], i["a07"])
+        varSearchData01.set(i["id"])
+        varSearchData02.set(i["a01"])
+        varSearchData03.set(i["a02"])
+        varSearchData04.set(i["a03"])
+        varSearchData05.set(i["a04"])
+        varSearchData06.set(i["a05"])
+        varSearchData07.set(i["a06"])
+
+        print(i["id"], i["a01"], i["a02"], i["a03"], i["a04"], i["a05"], i["a06"])
 
 
 # DisplayText
 buttonText: str = "搜尋"
-titleLabelText: str = "請輸入軟體名稱:"
+titleLabelText: str = "請選擇縣市名稱:"
 messageboxTitle: str = "message"
-errorMessage: str = "請輸入軟體名稱"
+errorMessage: str = "請輸入縣市名稱"
 
 # HostInfo
 databasePath: str = "//10.152.110.2/pcinfo/INPCTW-PCIESSDCN.html"
 databaseIP: str = "10.152.110.2"
 
 
-# FunctionArea
-def get_input_text():
-    if inputTextField.get() == "":
-        tk.messagebox.showerror(messageboxTitle, errorMessage)
-    else:
-        varSearchData.set(inputTextField.get())
-        get_api(inputTextField.get())
-        # canvas.forget()
-
-
 # View
 titleLabelTextLabel = tk.Label(windows, text=titleLabelText)
 inputTextField = ttk.Combobox(windows, values=dataArray)
 searchButton = tk.Button(windows, text=buttonText, height=1, command=get_input_text)
-displayTextLabel = tk.Label(windows, textvariable=varSearchData)
+displayTextLabel01 = tk.Label(windows, textvariable=varSearchData01)
+displayTextLabel02 = tk.Label(windows, textvariable=varSearchData02)
+displayTextLabel03 = tk.Label(windows, textvariable=varSearchData03)
+displayTextLabel04 = tk.Label(windows, textvariable=varSearchData04)
+displayTextLabel05 = tk.Label(windows, textvariable=varSearchData05)
+displayTextLabel06 = tk.Label(windows, textvariable=varSearchData06)
+displayTextLabel07 = tk.Label(windows, textvariable=varSearchData07)
+
 
 titleLabelTextLabel.place(x=10, y=15)
 inputTextField.place(x=120, y=15)
 searchButton.place(x=300, y=10)
-displayTextLabel.place(x=10, y=40)
+displayTextLabel01.place(x=0, y=40)
+displayTextLabel02.place(x=100, y=40)
+displayTextLabel03.place(x=200, y=40)
+displayTextLabel04.place(x=300, y=40)
+displayTextLabel05.place(x=400, y=40)
+displayTextLabel06.place(x=500, y=40)
+displayTextLabel07.place(x=600, y=40)
 
 # searchData
 testText = pd.read_html(databasePath)
